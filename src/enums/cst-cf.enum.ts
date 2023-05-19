a2129fbf75d3cb85be99277daa5655cd3c4b8ac4
pcfmello <pcfmello@gmail.com>
2023-05-19 19:10:27 -0300

    feat: Add cstcf enum

---
+++ src/enums/cst-cf.enum.ts
@@ -0,0 +1,13 @@
+export enum CstCfEnum {
+  CST_CSTCF_00 = "00",
+  CST_CSTCF_10 = "10",
+  CST_CSTCF_20 = "20",
+  CST_CSTCF_30 = "30",
+  CST_CSTCF_40 = "40",
+  CST_CSTCF_41 = "41",
+  CST_CSTCF_50 = "50",
+  CST_CSTCF_51 = "51",
+  CST_CSTCF_60 = "60",
+  CST_CSTCF_70 = "70",
+  CST_CSTCF_90 = "90",
+}
