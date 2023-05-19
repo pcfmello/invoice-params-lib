ecc28251dd24572204caa1dfa8bd76ad85bf1f2b
pcfmello <pcfmello@gmail.com>
2023-05-19 19:20:31 -0300

    feat: Add pis model

---
+++ src/models/pis.model.ts
@@ -0,0 +1,13 @@
+export interface PisModel {
+  cst: string;
+  clEnq: string;
+  cnpjProd: string;
+  cSelo: string;
+  qSelo: number;
+  cEnq: number;
+  vBc: number;
+  qUnid: number;
+  vUnid: number;
+  pIpi: number;
+  vIpi: number;
+}
