5fe90b2bdaeeee309b21baa245e56183f4bf7198
pcfmello <pcfmello@gmail.com>
2023-05-19 19:18:32 -0300

    feat: Add motivo-da-desoneracao-do-icms module

---
+++ src/lists/motivo-da-desoneracao-do-icms-st.list.ts
@@ -0,0 +1,61 @@
+import { MotivoDesoneracaoIcmsStEnum } from "../enums/motivo-da-desoneracao-do-icms-st.enum";
+import { MotivoDesoneracaoIcmsStModel } from "../models/motivo-da-desoneracao-do-icms-st.model";
+
+export const motivoDesoneracaoIcmsStList: MotivoDesoneracaoIcmsStModel[] = [
+  {
+    type: MotivoDesoneracaoIcmsStEnum.TAXI,
+    desc: "Taxi",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.DEFICIENTE_FISICO,
+    desc: "Deficiente Físico",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.PRODUTOR_AGROPECUARIO,
+    desc: "Produtor Agropecuário",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.FROTISTA_LOCADORA,
+    desc: "Frotista/Locadora",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.DIPLOMATICO_CONSULAR,
+    desc: "Diplomático/Consular",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.UTILITARIO_MOTOCICLETAS_AMAZONIA,
+    desc: "Utilitários e Motocicletas da Amazônia Ocidental e Áreas de Livre Comércio (Resolução 714/88 e 790/94 – CONTRAN e suas alterações)",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.SUFRAMA,
+    desc: "SUFRAMA",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.VENDA_ORGAOS_PUBLICOS,
+    desc: "Venda a Órgãos Públicos",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.OUTROS,
+    desc: "Outros",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.DEFICIENTE_CONDUTOR,
+    desc: "Deficiente Condutor (Convênio ICMS 38/12)",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.DEFICIENTE_NAO_CONDUTOR,
+    desc: "Deficiente Não Condutor (Convênio ICMS 38/12)",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.ORGAO_FOMENTO_DESENVOLVIMENTO_AGROPECUARIO,
+    desc: "Órgão de fomento e desenvolvimento agropecuário",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.OLIMPIADAS_RIO_2016,
+    desc: "Olimpíadas Rio 2016",
+  },
+  {
+    type: MotivoDesoneracaoIcmsStEnum.SOLICITADO_PELO_FISCO,
+    desc: "Solicitado pelo Fisco",
+  },
+];
---
+++ src/models/motivo-da-desoneracao-do-icms-st.model.ts
@@ -0,0 +1,6 @@
+import { MotivoDesoneracaoIcmsStEnum } from "../enums/motivo-da-desoneracao-do-icms-st.enum";
+
+export interface MotivoDesoneracaoIcmsStModel {
+  type: MotivoDesoneracaoIcmsStEnum;
+  desc: string;
+}
